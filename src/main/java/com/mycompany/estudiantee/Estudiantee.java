0fb51a9ae976532ae0c002afd9aadb34874a134c
David810-liga <david.quero875@ist17dejulio.edu.ec>
2025-11-07 11:57:00 -0500

    Inicio Proyecto Vacio

---
+++ src/main/java/com/mycompany/estudiantee/Estudiantee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estudiantee;
+
+/**
+ *
+ * @author User
+ */
+public class Estudiantee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

fc2cba773bf5998c46053436664565cf9030532b
David810-liga <david.quero875@ist17dejulio.edu.ec>
2025-11-07 12:28:07 -0500

    Clase Estudiante

--- src/main/java/com/mycompany/estudiantee/Estudiantee.java
+++ src/main/java/com/mycompany/estudiantee/Estudiantee.java
@@ -9,8 +9,61 @@ package com.mycompany.estudiantee;
  * @author User
  */
 public class Estudiantee {
+    private String nombre;
+    private String cedula;
+    private String telefono;
+    private String direccion;
+    
+   public Estudiantee(){
+   }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public Estudiantee(String nombre, String cedula, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos del Estudiantee"+"\n"+"Nombre: "+getNombre()+"\n"+
+                "Cedula: "+getCedula()+"\n"+
+                "Telefono: "+getTelefono()+"\n"+
+                "Direccion: "+getDireccion()+"\n"
+                ;
+
+    
     }
 }
---
+++ src/main/java/com/mycompany/estudiantee/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.estudiantee;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+        Estudiantee e=new Estudiantee("David", "1005069875", "0999466053", "Los Ceibos");
+        System.out.println(e.toString());
+        
+    }
+}
